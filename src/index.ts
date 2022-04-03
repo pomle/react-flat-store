b44ab3550cd8d3385132af4ad15042b1d31a4049
Pontus Alexander <pontus.alexander@gmail.com>
2022-04-03 18:55:25 +0200

    Export common types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export { useEntity } from "./entity";
 export { createStoreContext } from "./context";
 export { toList, withData } from "./helper";
+
+export type { Collection } from "./collection";
+export type { EntityStore } from "./entity";
+export type { Entry } from "./entries";
