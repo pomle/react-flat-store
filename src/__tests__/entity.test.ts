54a193a9e815c2fb8a9a3f028c34195f1c5e0b3b
Pontus Alexander <pontus.alexander@gmail.com>
2022-04-05 03:52:13 +0200

    Add test for useEntries

---
+++ src/__tests__/entity.test.ts
@@ -0,0 +1,44 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { act } from "react-test-renderer";
+import { useEntity } from "../entity";
+
+describe("useEntity", () => {
+  type Shape = {
+    id: string;
+  };
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  it("combines useEntries and useCollection", () => {
+    const a: Shape = {
+      id: "A",
+    };
+
+    const b: Shape = {
+      id: "B",
+    };
+
+    const hook = renderHook(() => useEntity<Shape>());
+
+    act(() => {
+      hook.result.current.entries.set("a", a);
+      hook.result.current.entries.set("b", b);
+
+      jest.advanceTimersByTime(150);
+
+      hook.result.current.collection.set("my-list", ["a", "b"]);
+
+      hook.rerender();
+    });
+
+    const entries = hook.result.current.collection.get("my-list");
+
+    expect(entries?.length).toBe(2);
+    expect(entries?.[0].ready).toBe(true);
+    expect(entries?.[0].data).toBe(a);
+    expect(entries?.[1].ready).toBe(true);
+    expect(entries?.[1].data).toBe(b);
+  });
+});
